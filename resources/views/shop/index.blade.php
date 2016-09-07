ff1c8ce96b77f4c6af489b830f28e2feb390b32b
Hugo Espinosa <vaporic@gmail.com>
2016-09-07 00:26:54 -0500

    first commit

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping Cart
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://www.thealmightyguru.com/Reviews/HarryPotter/Images/Cover-GobletOfFire.jpg" class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iure nam odio quisquam quod quos saepe soluta tenetur. Aliquam assumenda aut beatae corporis deleniti, exercitationem laudantium quidem temporibus unde voluptatibus?
+                    </p>
+                    <div>
+                        <a href="#" class="btn btn-default" role="button">Button</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
